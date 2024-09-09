3a15a6849b5ecebf9b25f741f3a4fbea50563cd6
AbhishekKushwaha-007 <149022331+AbhishekKushwaha-007@users.noreply.github.com>
2024-09-09 14:08:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "32467"
+accountCity = "Ghazipur"
+
+// accountId = 34342. Not Allowed
+
+accountEmail = "abhi@gmail.com"
+accountPassword = "3327"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
